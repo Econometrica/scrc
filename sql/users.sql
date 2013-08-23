399fd50dfcabd85136f7f79570df39616baa50c2
Pat Cappelaere <pat@cappelaere.com>
2013-08-23 16:46:52 -0400

    Update users table

--- sql/users.sql
+++ sql/users.sql
@@ -12,7 +12,7 @@
  Target Server Version : 90204
  File Encoding         : utf-8
 
- Date: 08/18/2013 08:48:50 AM
+ Date: 08/23/2013 16:46:33 PM
 */
 
 -- ----------------------------
@@ -22,7 +22,6 @@ DROP TABLE IF EXISTS "public"."users";
 CREATE TABLE "public"."users" (
 	"id" int4 NOT NULL DEFAULT nextval('users_id_seq'::regclass),
 	"username" varchar NOT NULL COLLATE "default",
-	"password" varchar NOT NULL COLLATE "default",
 	"site_id" int4 NOT NULL,
 	"email" varchar NOT NULL COLLATE "default",
 	"md5" varchar COLLATE "default"
@@ -34,11 +33,11 @@ ALTER TABLE "public"."users" OWNER TO "postgres";
 --  Records of users
 -- ----------------------------
 BEGIN;
-INSERT INTO "public"."users" VALUES ('0', 'admin', 'admin', '0', 'pat@me.com', null);
-INSERT INTO "public"."users" VALUES ('1', 'cms', 'cms', '0', 'pat@cappelaere.com', '7a141135be059d091c78740f108d8770');
-INSERT INTO "public"."users" VALUES ('2', 'site1', 'site1', '10', 'patrice@vightel.com', 'c7e6442e93e68a5624c004f4cdd08b38');
-INSERT INTO "public"."users" VALUES ('3', 'site2', 'site2', '12', 'pat@geobliki.com', null);
-INSERT INTO "public"."users" VALUES ('4', 'site3', 'site3', '13', 'cappelaere@gamil.com', null);
+INSERT INTO "public"."users" VALUES ('1', 'cms', '0', 'pat@cappelaere.com', 'adfd30c303816f8a403b53c405b9a4f5');
+INSERT INTO "public"."users" VALUES ('0', 'admin', '0', 'pat@me.com', '69d53e07abdece8b72e07f1f77ae961c');
+INSERT INTO "public"."users" VALUES ('2', 'site10', '10', 'patrice@vightel.com', '99ab116a9acd7b1f57fc913839aec664');
+INSERT INTO "public"."users" VALUES ('3', 'site12', '12', 'pat@geobliki.com', 'a7df3b9cecd11b0fd48f99ab23c50278');
+INSERT INTO "public"."users" VALUES ('4', 'site13', '13', 'cappelaere@gmail.com', '3cd06afc777b93a65d1326a08c5e7bfc');
 COMMIT;
 
 -- ----------------------------
@@ -55,5 +54,4 @@ ALTER TABLE "public"."users" ADD CONSTRAINT "email" UNIQUE ("username","id","ema
 --  Indexes structure for table users
 -- ----------------------------
 CREATE UNIQUE INDEX  "email" ON "public"."users" USING btree(username COLLATE "default" ASC NULLS LAST, "id" ASC NULLS LAST, email COLLATE "default" ASC NULLS LAST);
-CREATE UNIQUE INDEX  "email_index" ON "public"."users" USING btree(username COLLATE "default" ASC NULLS LAST, "id" ASC NULLS LAST, email COLLATE "default" ASC NULLS LAST);
 
